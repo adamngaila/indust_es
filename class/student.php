7da87ef73c42ece5da5a3ebf257d7a033c1a0c2b
adamngaila <38142076+adamngaila@users.noreply.github.com>
2019-07-07 09:23:54 +0300

    Update student.php

--- class/student.php
+++ class/student.php
@@ -4,10 +4,10 @@ class Student
     private $conn;
     function __construct() {
     session_start();
-    $servername = "localhost";
-    $dbname = "indust";
-    $username = "root";
-    $password = "";
+    $servername = "bbj31ma8tye2kagi.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+    $dbname = "xdvjkpkp986nwt21";
+    $username = "zx0hce4ovc2os4cs";
+    $password = "x2den1myinx55q73";
   
 
     // Create connection
