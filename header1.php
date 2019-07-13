35eae46cf27f8fe2b0d52f5eb9a0235f504fab70
adamngaila <38142076+adamngaila@users.noreply.github.com>
2019-07-13 11:39:01 +0300

    Update header1.php

--- header1.php
+++ header1.php
@@ -160,7 +160,10 @@
                 </div>
                 <div class="col-lg-3">
                     <div class="alert alert-warning text-center">
-                      <i class="fa  fa-pencil fa-3x">   <a href="lighting.php"></i>&nbsp; Energy audit
+                      <i class="fa  fa-pencil fa-3x"> </i>&nbsp; Energy audit
+                        li>
+                                <a href="lighting.php">Lighting</a>
+                            </li>
                     </div>
                 </div>
                 <!--end quick info section -->
