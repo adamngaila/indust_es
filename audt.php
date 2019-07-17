bdff7bdf5a6a4c6e0e2a2025a197916eb4b53c97
adamngaila <38142076+adamngaila@users.noreply.github.com>
2019-07-18 00:20:32 +0300

    Add files via upload

---
+++ audt.php
@@ -0,0 +1,60 @@
+<?php
+include 'header1.php';
+if (isset($_POST['create_consu'])) {
+    $student_obj->create_consu_info($_POST);
+}
+?>
+<hr>
+<div class="container "> 
+    <div class="row content ">
+        <h3>ENERGY AUDITING</h3>
+        <div> 
+        </div>
+
+        <P> energy audit is one of the first tasks to be performed in the accomplishment of an effective energy cost control program. An energy audit consists of a detailed examination of how a facility uses energy, what the facility pays for that energy, and finally, a recommended program for changes in operating practices or energy-consuming equipment that will cost-effectively save dollars on energy bills. The energy audit is sometimes called an energy survey or an energy analysis, so that it is not hampered with the negative connotation of an audit in the sense of an IRS audit. The energy audit is a positive experience with significant benefits to the business or individual, and the term “audit” should be avoided if it clearly produces a negative image in the mind of a particular business or individual.</P>
+     
+        
+    </div>
+</div>
+<hr/>
+<?php
+include 'footer.php';
+?>
+
+
+                
+                </div>
+
+
+            </div>
+
+
+                    </div>
+                    <!--End Chat Panel Example-->
+                </div>
+            </div>
+
+
+         
+
+
+        </div>
+        <!-- end page-wrapper -->
+
+    </div>
+    <!-- end wrapper -->
+
+    <!-- Core Scripts - Include with every page -->
+    <script src="assets/plugins/jquery-1.10.2.js"></script>
+    <script src="assets/plugins/bootstrap/bootstrap.min.js"></script>
+    <script src="assets/plugins/metisMenu/jquery.metisMenu.js"></script>
+    <script src="assets/plugins/pace/pace.js"></script>
+    <script src="assets/scripts/siminta.js"></script>
+    <!-- Page-Level Plugin Scripts-->
+    <script src="assets/plugins/morris/raphael-2.1.0.min.js"></script>
+    <script src="assets/plugins/morris/morris.js"></script>
+    <script src="assets/scripts/dashboard-demo.js"></script>
+
+</body>
+
+</html>
